125d504b954f3f57b4efe6d353f902c70b4b5803
arshithathota2005-ui <arshithathota2005@gmail.com>
2026-09-03 15:01:59 +0530

    Add files via upload

---
+++ variables2.py
@@ -0,0 +1,114 @@
+Python 3.14.4 (v3.14.4:23116f998f6, Apr  7 2026, 09:45:22) [Clang 17.0.0 (clang-1700.6.4.2)] on darwin
+Enter "help" below or click "Help" above for more information.
+a=2
+print(a)
+2
+b=13
+print(a+b)
+15
+e=100
+print(E)
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    print(E)
+NameError: name 'E' is not defined. Did you mean: 'e'?
+print(e)
+100
+h=120
+print(h)
+120
+name="arshi"
+print(name)
+arshi
+print("name")
+name
+city="IBM"
+print(city)
+IBM
+0a=1
+SyntaxError: invalid decimal literal
+a1=3
+print(a1)
+3
+a1234=1
+print(a1234)
+1
+$=9
+SyntaxError: invalid syntax
+a$=8
+SyntaxError: invalid syntax
+_a=9
+print(_a)
+9
+>>>  a=10
+...  
+SyntaxError: unexpected indent
+>>> a=10;b=20
+>>> print(a+b)
+30
+>>> a=10,b=20
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> a,b=10,20
+>>> print(a+b)
+30
+>>> a=1,2,3,4,6,7
+>>> print(a)
+(1, 2, 3, 4, 6, 7)
+>>> a,b,c=1,2,3
+>>> print(a,b,c)
+1 2 3
+>>> a,b,c=1,2,3,4,5,6
+Traceback (most recent call last):
+  File "<pyshell#33>", line 1, in <module>
+    a,b,c=1,2,3,4,5,6
+ValueError: too many values to unpack (expected 3, got 6)
+>>> first name="lkjh"
+SyntaxError: invalid syntax
+>>> first_name="arshi"
+>>> print(first_name)
+arshi
+>>> firstname="thota"
+>>> print(firstname)
+thota
+>>> fname="arshi"
+>>> lname"thota"
+SyntaxError: invalid syntax
+>>> lname="thota"
+>>> print(fname+lname)
+arshithota
+>>> print(fname+" "+lname)
+arshi thota
+>>> g=10
+>>> print(g)
+10
+>>> del(g)
+>>> print(g)
+Traceback (most recent call last):
+  File "<pyshell#47>", line 1, in <module>
+    print(g)
+NameError: name 'g' is not defined
+>>> food="egg"
+>>> print(FOOD)
+Traceback (most recent call last):
+  File "<pyshell#49>", line 1, in <module>
+    print(FOOD)
+NameError: name 'FOOD' is not defined
+>>> print(Food)
+Traceback (most recent call last):
+  File "<pyshell#50>", line 1, in <module>
+    print(Food)
+NameError: name 'Food' is not defined. Did you mean: 'food'?
+>>> print(food)
+egg
+>>> a=10
+>>> print(a)
+10
+>>> b=13
+>>> print(a)
+10
+>>> if=123
+SyntaxError: invalid syntax
+while=432
+SyntaxError: invalid syntax
+del=8
+SyntaxError: invalid syntax
