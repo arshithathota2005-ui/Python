d283864f97ade7cafb7ba28bf1b4837907ba2870
arshithathota2005-ui <arshithathota2005@gmail.com>
2026-09-10 21:26:29 +0530

    Add files via upload

---
+++ runtime_input.py
@@ -0,0 +1,133 @@
+'''a=10
+b=20
+print(a+b)'''
+
+'''a=10
+b=20
+c=a+b
+print(c)'''
+
+#print(4+5)
+
+
+'''a = int(input("enter a "))
+b = int(input("enter b "))
+print(a+b)'''
+
+'''a = float(input("enter a "))
+b = float(input("enter b "))
+print(a+b)'''
+
+'''a = str(input("enter a "))
+b = str(input("enter b "))
+print(a+b)'''
+
+'''a = input("enter a ")
+b = input("enter b ")
+print(a+b)'''
+
+
+'''fname=input("First name")
+lname=input("Last name")
+print((fname+" "+lname).title())'''
+
+
+'''a = complex(input("enter a "))
+b = complex(input("enter b "))
+print(a+b)'''
+
+
+'''a = bool(input("enter a "))
+b = bool(input("enter b "))
+c = bool(input("enter c "))
+print(a+b+c)'''
+
+'''a=list[input()]
+print(a)'''
+
+
+
+'''a=set[input()]
+print(a)'''
+
+'''a=dict[input()]
+print(a)'''
+
+'''a=int(input())
+b=int(input())
+options = int(input("Choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+'''a=int(input())
+b=int(input())
+options = int(input(Choose the option
+                    1.add
+                    2.sub
+                    3.mul))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+'''a=int(input())
+b=int(input())
+options = input("Choose the option add sub mul")
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a = int(input())
+b = int(input())
+print(a+b)'''
+
+
+
+'''idno = int(input("enter id"))
+name = input("enter name:")
+moblie = int(input("enter mobile"))
+mail = input("enter mail")
+clg = input("enter college:")
+branch = input("enter branch:")
+print("    *Student Profile*      ")
+print(f"Id: {idno}")
+print(f"Name: {name}")
+print(f"mobile: {moblie}")
+print(f"mail: {mail}")
+print(f"college: {clg}")
+print(f"branch: {branch}")'''
+
+
+'''a=[10,20,30,40,50]
+a.insert(2,25)
+print(a)'''
+
+
+[[[[[[[[[[[[[[[[[[[][[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]-----------------------------------------------------------------------
+'''a=["python","java","c"]
+a.extend(["c","o","d","e"])
+print(a)'''
+'''
+a=["python","java","c"]
+a.extend("code")
+print(a)'''
+
+'''a=("apple","banana","mango")
+b=list(a)
+b.append("kiwi")
+c=tuple(b)
+print(c)'''
+
+'''a={"black","White","red"}
+a.remove("White")
+print(a)'''
+
+a={"name":"arshi","city":"ibm"}
+a.update({"state":"ap"})
+print(a)
+
+             
+
+
